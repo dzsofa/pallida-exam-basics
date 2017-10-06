cf1ce142f2eee434d86083bc133e903ac4473948
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-06 11:48:40 +0200

    Pallida Basic Exam, CandyShop createSweets() method done

--- candyshop/src/CandyShop.java
+++ candyshop/src/CandyShop.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
+
 public class CandyShop {
     int sugar;
     int income;
+    ArrayList candyShop;
 
     public CandyShop(int sugar) {
         this.sugar = sugar;
         this.income = 0;
+        this.candyShop = new ArrayList();
+    }
+
+    public void createSweets(Sweets sweetType) {
+        if (sweetType.sugarContent == 10) {
+            candyShop.add(new Candy());
+            this.sugar -= 10;
+        } else if (sweetType.sugarContent == 5) {
+            candyShop.add(new Lollipop());
+            this.sugar -= 5;
+        }
     }
 }
