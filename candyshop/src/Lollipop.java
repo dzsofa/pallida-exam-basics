fa74b622fb3882a67ca85602570ba6dc861d169f
Zsofia <zsofi.dedeo@mensa.hu>
2017-10-06 12:08:09 +0200

    Pallida Basic Exam, got rid of unused classes

--- candyshop/src/Lollipop.java
+++
@@ -1,7 +0,0 @@
-public class Lollipop extends Sweets {
-
-    public Lollipop() {
-        this.price = 10;
-        this.sugarContent = 5;
-    }
-}
--- candyshop/src/Sweets.java
+++
@@ -1,8 +0,0 @@
-public class Sweets {
-    int price;
-    int sugarContent;
-
-    public Sweets() {
-
-    }
-}
